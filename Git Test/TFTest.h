790e8cd29a093e71fee2d7732fb260e6e0ea2bee
Colin Dickson <dicksoncc@gmail.com>
2014-03-04 16:53:27 -0600

    added a test class

---
+++ Git Test/TFTest.h
@@ -0,0 +1,13 @@
+//
+//  TFTest.h
+//  Git Test
+//
+//  Created by Colin Dickson on 3/4/14.
+//  Copyright (c) 2014 Tekfyre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TFTest : NSObject
+
+@end
---
+++ Git Test/TFTest.m
@@ -0,0 +1,13 @@
+//
+//  TFTest.m
+//  Git Test
+//
+//  Created by Colin Dickson on 3/4/14.
+//  Copyright (c) 2014 Tekfyre. All rights reserved.
+//
+
+#import "TFTest.h"
+
+@implementation TFTest
+
+@end
